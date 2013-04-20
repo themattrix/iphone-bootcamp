7bed82f1dca8ed0f14b8e824d23a2c6272e34c9c
Matthew Tardiff <mattrix@gmail.com>
2013-04-20 15:59:33 -0500

    Added comments to ViewController.m.

--- assignments/06_Table/Table/ViewController.m
+++ assignments/06_Table/Table/ViewController.m
@@ -16,10 +16,19 @@
 
 - (void)viewDidLoad
 {
-    // if you dare
-    self.mList = [[NSArray alloc] initWithObjects:@"Windows 3.11", @"Windows 95", @"Windows 98", @"Windows ME", @"Windows NT", @"Windows 2000", @"Windows XP", @"Windows Vista", @"Windows 7", nil];
-    
     [super viewDidLoad];
+
+    self.mList = [[NSArray alloc] initWithObjects:
+                  @"Windows 3.11",
+                  @"Windows 95",
+                  @"Windows 98",
+                  @"Windows ME",
+                  @"Windows NT",
+                  @"Windows 2000",
+                  @"Windows XP",
+                  @"Windows Vista",
+                  @"Windows 7",
+                  nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +44,24 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *tableId = @"tableID";
+    /* This creates a new cells as they come into view. */
     
+    // this can be anything unique to this table (and we only have one table)
+    static NSString *tableId = @"tableId";
+    
+    // determine if there were any out-of-view cells that we can reuse
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableId];
     
-    if (cell == nil) {
+    // if no cells are available to reuse, create a new cell
+    if (cell == nil)
+    {
+        // create with 'reuseIdentifier' so that it's stored in the cell cache by an identifier
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableId];
     }
     
-    NSUInteger row = [indexPath row];
+    /* Always reset all of the cell properties so that reused cells don't show up with stale data. */
+    
+     NSUInteger row = [indexPath row];
     cell.textLabel.text = [self.mList objectAtIndex:row];
     return cell;
 }
